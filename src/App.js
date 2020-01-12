7a1a32928603951d357b7b77da91b49727935255
John <lisney@naver.com>
2020-01-12 21:19:42 +0900

    react 구조화

--- src/App.js
+++ src/App.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import soo from './soo.jpg'
 import './App.css';
+import Customer from './components/Customer';
+
+const customers =[
+  {
+    'id':1,
+    'image':'https://placeimg.com/64/64/1',
+    'name':'박지',
+    'birthday':"951212",
+    'gender':'여자',
+    'job': '모델'
+  },
+  {
+    'id':3,
+    'image':'https://placeimg.com/64/64/2',
+    'name':'수지',
+    'birthday':"951212",
+    'gender':'여자',
+    'job': '모델'
+  },
+  {
+    'id':2,
+    'image':'https://placeimg.com/64/64/3',
+    'name':'고지',
+    'birthday':"951212",
+    'gender':'남자',
+    'job': '모델'
+  }
+];  
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={soo} className="App-logo" alt="logo" />
-          <p className="Grayscale">
-            Hello React App!
-          </p>
-          
-        </header>
+        {
+          customers.map(c=>{
+            return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job}/>
+          })
+        }
       </div>
     );
   }
