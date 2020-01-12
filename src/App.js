85d141659605e987e58c04d92131521a7b7e6127
John <lisney@naver.com>
2020-01-12 22:20:05 +0900

    머티리얼UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import soo from './soo.jpg'
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 const customers =[
   {
@@ -35,11 +40,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          customers.map(c=>{
-            return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job}/>
-          })
-        }
+        <Table>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {customers.map((c,i)=>(<Customer key={i} id={c.id} image={c.image} name={c.name} birthday={c.birthday} job={c.job}/>))}
+
+          </TableBody>
+        </Table>
       </div>
     );
   }
